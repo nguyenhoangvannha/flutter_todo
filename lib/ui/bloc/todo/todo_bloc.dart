7f9fa46ce90b5ab36ec42367c45f7334e11944dd
Nha NHV <nguyenhoangvannha@gmail.com>
2019-12-09 15:55:28 +0700

    fix: prevent reload all to do when one todo update

--- lib/ui/bloc/todo/todo_bloc.dart
+++ lib/ui/bloc/todo/todo_bloc.dart
@@ -15,30 +15,33 @@ class TodoBloc extends Bloc<TodoEvent, TodoState> {
   @override
   TodoState get initialState => InitialTodoState();
 
+  List<Todo> _listTodo = [];
+
   @override
   Stream<TodoState> mapEventToState(
     TodoEvent event,
   ) async* {
     if (event is FetchListTodo) {
-      yield FetchingListTodo();
-      var res = await _todoRepo.getListTodo();
-      switch (res.type) {
-        case ResourceType.Error:
-          yield FetchListTodoError(res.exception);
-          break;
-        case ResourceType.Success:
-          final List<Todo> _listTodo = res.data;
-          var _listCompletedTodo = _listTodo.where((todo) {
-            return todo.completed;
-          }).toList();
-          var _listInCompletedTodo = _listTodo.where((todo) {
-            return !todo.completed;
-          }).toList();
-          yield FetchListTodoResult(
-              listAllTodo: _listTodo,
-              listCompleteTodo: _listCompletedTodo,
-              listIncompleteTodo: _listInCompletedTodo);
-          break;
+      if (_listTodo != null && _listTodo.isNotEmpty) {
+        yield FetchListTodoResult(
+            listAllTodo: _listTodo,
+            listCompleteTodo: _getListTodoByType(_listTodo, true),
+            listIncompleteTodo: _getListTodoByType(_listTodo, false));
+      } else {
+        yield FetchingListTodo();
+        var res = await _todoRepo.getListTodo();
+        switch (res.type) {
+          case ResourceType.Error:
+            yield FetchListTodoError(res.exception);
+            break;
+          case ResourceType.Success:
+            _listTodo = res.data;
+            yield FetchListTodoResult(
+                listAllTodo: _listTodo,
+                listCompleteTodo: _getListTodoByType(_listTodo, true),
+                listIncompleteTodo: _getListTodoByType(_listTodo, false));
+            break;
+        }
       }
     }
     if (event is AddTodo) {
@@ -49,6 +52,7 @@ class TodoBloc extends Bloc<TodoEvent, TodoState> {
           yield AddTodoError(res.exception);
           break;
         case ResourceType.Success:
+          _listTodo.insert(0, event.todo);
           yield AddTodoResult();
           break;
       }
@@ -60,6 +64,13 @@ class TodoBloc extends Bloc<TodoEvent, TodoState> {
           yield UpdateTodoError(res.exception);
           break;
         case ResourceType.Success:
+          _listTodo.firstWhere((todo) {
+            if (todo.id == event.todo.id) {
+              todo.completed = event.todo.completed;
+              return true;
+            }
+            return false;
+          });
           add(FetchListTodo());
           yield UpdateTodoResult();
           break;
@@ -72,10 +83,19 @@ class TodoBloc extends Bloc<TodoEvent, TodoState> {
           yield UpdateTodoError(res.exception);
           break;
         case ResourceType.Success:
+          _listTodo.removeWhere((m) {
+            return m.id == event.todo.id;
+          });
           add(FetchListTodo());
           yield UpdateTodoResult();
           break;
       }
     }
+  }
+
+  List<Todo> _getListTodoByType(List<Todo> listTodo, bool _completed) {
+    return listTodo.where((todo) {
+      return todo.completed == _completed;
+    }).toList();
   }
 }
